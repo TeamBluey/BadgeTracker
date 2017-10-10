bd19b246226cda485b9f388a20517c42791d66f3
Steven Childrey <steven.childrey36@yahoo.com>
2017-10-09 21:59:54 -0400

    Add register page

---
+++ register.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<html lang='en'>
+<head>
+    <title>Badge Tracker | Register</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="register.css" rel="stylesheet" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-offset-3 col-sm-6 col-xs-12 loginContainer">
+                <form action='' method='POST'>
+                    <h3 class='title'>Register</h3>
+                    <!-- 
+                        Need
+                        SID
+                        Username
+                        FName
+                        LName
+                        PW
+                        ClassRank
+                    -->
+                    <input type='email' name='email' placeholder='Email Address' class='typeInputs'>
+                    <input type='number' name='SID' placeholder='ID Number' class='typeInputs'>
+                    <input type="text" name="FName" placeholder='First Name' class='typeInputs'>
+                    <input type="text" name="LName" placeholder='Last Name' class='typeInputs'>
+                    <input type="password" name="pwd" placeholder='Password' class='typeInputs'>
+                    <input type="password" name="pwdConfirm" placeholder='Confirm Password' class='typeInputs'>
+                    <select name="" id="" class='typeInputs'>
+                        <option value="Freshman">Freshman</option>
+                        <option value="Sophomore">Sophomore</option>
+                        <option value="Junior">Junior</option>
+                        <option value="Senior">Senior</option>
+                    </select>
+                    <input type="submit" value="Register" class='submit'>
+                    <hr>
+                    <p>Already have an Account?<br/><a href='login.php'>Log In</a></p>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
