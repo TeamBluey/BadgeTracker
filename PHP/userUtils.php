b51e0779adfd4a000aa2f1b43699f70afb5d3822
Jeffrey Beebe <jbeebe@radford.edu>
2017-10-23 18:45:27 -0400

    Reworking Queries

--- PHP/userUtils.php
+++ PHP/userUtils.php
@@ -3,22 +3,22 @@ error_reporting(E_ALL);
 require_once('../SQL/dbConn.php');
 
 function getUserInitials( $userId ) {
-	$query = "SELECT FName,LName FROM Students WHERE SID = '".$userId."'";
+	$query = "SELECT FName,LName FROM Students WHERE (SID = '$userId')";
 	$result = mysqli_query($connection, $query);
 	$row = mysqli_fetch_assoc($result);
 	
-	$fname = $row["FName"];
-	$lname = $row["LName"];
+	$fname = $row['FName'];
+	$lname = $row['LName'];
 	
-	$initials = $fname[0] . $lname[0];
+	$initials = "$fname[0]" . "$lname[0]";
 	return $initials;
 }
 
 function getUsername( $userId ) {
-	$query = "SELECT Username FROM Students WHERE SID = '".$userId."'";
+	$query = "SELECT Username FROM Students WHERE (SID = '$userId')";
 	$result = mysqli_query($connection, $query);
 	$row = mysqli_fetch_assoc($result);
-	return $row["Username"];
+	return $row['Username'];
 }
 
 function getNumBadges( $userId ) {
@@ -26,10 +26,10 @@ function getNumBadges( $userId ) {
 }
 
 function getScore( $userId ) {
-	$query = "SELECT Points FROM Students WHERE SID = '".$userId."'";
+	$query = "SELECT Points FROM Students WHERE (SID = '$userId')";
 	$result = mysqli_query($connection, $query);
 	$row = mysqli_fetch_assoc($result);
-	return $row["Points"];
+	return $row['Points'];
 }
 
 function loadEarnedBadges( $userId ) {
