9615352588a217c6388b6d61abd8661293b75583
Jeffrey Beebe <jbeebe@radford.edu>
2017-10-23 17:14:46 -0400

    Updating DB Functionality

--- PHP/userUtils.php
+++ PHP/userUtils.php
@@ -18,9 +18,7 @@ function getUsername( $userId ) {
 	$query = "SELECT Username FROM Students WHERE SID = '".$userId."'";
 	$result = mysqli_query($connection, $query);
 	$row = mysqli_fetch_assoc($result);
-	
-	$userName = $row["Username"];
-	return $userName;
+	return $row["Username"];
 }
 
 function getNumBadges( $userId ) {
