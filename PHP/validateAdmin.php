7f7d13f379e7594505238df951c913c330556f65
Jeffrey Beebe <jbeebe@radford.edu>
2017-11-14 14:54:39 -0500

    Admin login now functioning

--- PHP/validateAdmin.php
+++ PHP/validateAdmin.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-require_once('../SQL/dbConn.php');
+require_once('../../../conn.php');
 session_start();
 if( isset($_POST['email']) && isset($_POST['pwd']) ) {
 	// store and sanitize user input
@@ -14,18 +14,22 @@ if( isset($_POST['email']) && isset($_POST['pwd']) ) {
 		$pwd = trim($pwd);
 		$pwd = strip_tags($pwd);
 		$pwd = htmlspecialchars($pwd);
-	$pwdHash = hash("sha256", $pwd);
+	// $pwdHash = hash("sha256", $pwd);
 	
 	// create query and fetch result
-	$query = "SELECT Password FROM Admin WHERE Email = '".$email."'";
-	$result = mysqli_query($connection, $query);
-	$row = mysqli_fetch_assoc($result);
-	if( $pwdHash === $row["Password"] ) {
-		$_SESSION['uname'] = $email;
-		header("Location: admin.php");
+	$query = "SELECT Password FROM Admin WHERE Username = '".$email."'";
+	
+	if ( $result = mysqli_query($connection, $query) ) {
+		while ( $row = mysqli_fetch_assoc($result) ) {
+			if( $pwd === $row["Password"] ) {
+        		$_SESSION['uname'] = $email;
+        		header("Location: admin.php");
+    		}
+    		else
+        		echo "Invalid Email or Password";
+
+		}
 	}
-	else
-		echo "Invalid Email or Password";
 }
 mysqli_close($connection);
 ?>
