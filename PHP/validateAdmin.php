ea9a4d636b704694e291cc34fec8614b8d1baa68
jeffrey-beebe <jbeebe@radford.edu>
2017-11-14 14:26:54 -0500

    Added administrator validation handler

---
+++ PHP/validateAdmin.php
@@ -0,0 +1,31 @@
+<?php
+error_reporting(E_ALL);
+require_once('../SQL/dbConn.php');
+session_start();
+if( isset($_POST['email']) && isset($_POST['pwd']) ) {
+	// store and sanitize user input
+	$email = $_POST['email'];
+		$email = mysqli_real_escape_string($connection, $email);
+		$email = trim($email);
+		$email = strip_tags($email);
+		$email = htmlspecialchars($email);
+	$pwd = $_POST['pwd'];
+		$pwd = mysqli_real_escape_string($connection, $pwd);
+		$pwd = trim($pwd);
+		$pwd = strip_tags($pwd);
+		$pwd = htmlspecialchars($pwd);
+	$pwdHash = hash("sha256", $pwd);
+	
+	// create query and fetch result
+	$query = "SELECT Password FROM Admin WHERE Email = '".$email."'";
+	$result = mysqli_query($connection, $query);
+	$row = mysqli_fetch_assoc($result);
+	if( $pwdHash === $row["Password"] ) {
+		$_SESSION['uname'] = $email;
+		header("Location: admin.php");
+	}
+	else
+		echo "Invalid Email or Password";
+}
+mysqli_close($connection);
+?>
