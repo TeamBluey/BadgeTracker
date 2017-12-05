090f2ccaec45749638045e7335758ca465f89814
Steven Childrey <schildrey2@radford.edu>
2017-12-04 22:50:48 -0500

    Add backend for confirm and deny badges

---
+++ PHP/conDenyPending.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once("../SQL/dbConn.php");
+    $pendingInfo = $_POST["pendInfo"];
+    $values = explode("-", $pendingInfo);
+
+    if ($values[0] == "CONFIRM"){
+        $confirmQuery = "UPDATE proj3.Student_Badges SET Status='Achieved' WHERE SID=" . $values[1] . " && BID=" . $values[2];
+        if($result = mysqli_query($connection, $confirmQuery)){
+            header("Location: admin.php");
+        }
+    }
+    elseif($values[0] == "DENY"){
+        $denyQuery = "UPDATE proj3.Student_Badges SET Status='Denied' WHERE SID=" . $values[1] . " && BID=" . $values[2];
+        if($result = mysqli_query($connection, $denyQuery)){
+            header("Location: admin.php");
+        }
+    }
+    else{
+        echo "There was an error, go back to try again";
+    }
+
+?>
